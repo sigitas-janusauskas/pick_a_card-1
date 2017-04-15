64d245ea864b4efd664b69108ae03297cd4335a1
Vadim Venediktov <install.vv@gmail.com>
2017-04-15 17:35:09 +0300

    Some codestyle improvements

--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
 # encoding: utf-8
-puts 'Pick a Card. (c) goodprogrammer.ru'
 
 # Подключаем класс колоды
 require_relative 'lib/deck'
+
+# Выводим приветствие
+puts 'Pick a Card. (c) goodprogrammer.ru'
+puts
 
 # Создаем новую колоду и сразу её перемешиваем
 deck = Deck.new.shuffle
@@ -12,6 +15,7 @@ puts 'How many cards do you pick?'
 number = STDIN.gets.to_i
 
 # Нужное число раз выкидываем из колоды последню карту
+puts
 number.times do
   puts deck.pop_a_card
 end
