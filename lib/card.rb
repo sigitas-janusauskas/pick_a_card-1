c1f8a98781a7d8bc21ce7143762d87877e9b3b0a
Vadim Venediktov <install.vv@gmail.com>
2018-11-19 22:29:34 +0300

    Moved VALUES & SUITS constants to deck.rb

--- lib/card.rb
+++ lib/card.rb
@@ -3,12 +3,6 @@
 # Может сгенерить карту с заданными параметрами в т.ч. джокера и показать
 # её в удобном виде.
 class Card
-  # Массивы с наборами мастей и достоинств
-  VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A).freeze
-
-  # Буби, Черви, Крести, Пики
-  SUITS = %w(Diamonds Hearts Clubs Spades).freeze
-
   # Создаем новую карту с заданными параметрами
   def initialize(value, suit, joker = false)
     @value = value
--- lib/deck.rb
+++ lib/deck.rb
@@ -6,13 +6,19 @@ require_relative 'card'
 # Может сгенерить колоду с джокерами, перетасовать её выкинуть из колоды
 # произвольную или последнюю карту
 class Deck
+  # Массивы с наборами мастей и достоинств
+  VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A).freeze
+
+  # Буби, Черви, Крести, Пики
+  SUITS = %w(Diamonds Hearts Clubs Spades).freeze
+
   # Создаем новую колоды
   def initialize
     @cards = []
 
     # Берем из класса карты все возможные масти и достоинства
-    Card::VALUES.each do |value|
-      Card::SUITS.each do |suit|
+    VALUES.each do |value|
+      SUITS.each do |suit|
         @cards << Card.new(value, suit)
       end
     end
