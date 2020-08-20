aaad8a60b9b23005ce880d2376210651b9e2a00a
Sigitas_Janusauskas <ramahari108@gmail.com>
2020-08-20 22:08:59 +0300

    pridejau po puts programos galimybes

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ require_relative 'lib/deck'
 # Выводим приветствие
 # apie progr pradzia
 puts 'Pick a Card. (c) goodprogrammer.ru'
-puts
+puts " programa sudetinga"
 
 # Создаем новую колоду и сразу её перемешиваем
 deck = Deck.new.shuffle
